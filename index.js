0f7ab6609292342ce1719412e545204807fa51fc
Skyline300 <79264371+Skyline300@users.noreply.github.com>
2022-05-04 15:42:56 +0800

    v

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var folders = fs.readdirSync(path).filter(function (file) {
     return fs.statSync(path+'/'+file).isDirectory();
 });
 folders.forEach(element => {
-    var commandFiles = fs.readdirSync(`${mainDir}/modules/${element}`).filter(file => file.endsWith('.js'));
+    var commandFiles = fs.readdirSync(`${mainDir}/modules/${element}`).filter(file => file.endsWith('.js') && !file.startsWith("index"));
     for (const file of commandFiles) {
         delete require.cache[require.resolve(`${mainDir}/modules/${element}/${file}`)];
         const command = require(`${mainDir}/modules/${element}/${file}`);
@@ -23,18 +23,17 @@ folders.forEach(element => {
 
 
 // The startup information for the project, here you can add stuff that might be nice to see when the app starts
-// mapFunctions.get("Startup_function").function()
+mapFunctions.get("Startup_function").function()
 console.log("------------------------------------ Status ------------------------------------");
 console.log(__dirname);
 console.log(platform);
 console.log(`The Startup took ${new Date() - start}ms`)
 console.log(`${mapFunctions.size} Function modules loaded`);
-console.log(mapFunctions)
 console.log("--------------------------------------------------------------------------------");
 
 
 
- 
+
 
 
 
@@ -50,10 +49,4 @@ rl.on("line", data =>{
     const args = data.trim().split(" ");
     const command = args.shift().toLowerCase();
     mapFunctions.get("cliCommands").function(command, args)
-})
-
-
-
-process.on("uncaughtException", (err) => {
-    console.error(err.stack);
 })
--- modules/utility/cliCommands.js
+++ modules/utility/cliCommands.js
@@ -13,7 +13,7 @@ module.exports = {
                     return fs.statSync(path+'/'+file).isDirectory();
                 });
                 folders.forEach(element => {
-                    var commandFiles = fs.readdirSync(`${path}/${element}`).filter(file => file.endsWith('.js'));
+                    var commandFiles = fs.readdirSync(`${path}/${element}`).filter(file => file.endsWith('.js') && !file.startsWith("index"));
                     for (const file of commandFiles) {
                         delete require.cache[require.resolve(`${path}/${element}/${file}`)];
                         const command = require(`${path}/${element}/${file}`);
