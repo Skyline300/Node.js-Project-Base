165f3c2bb534a3036edaddb2dfe5b5902af53b84
Skyline300 <marcushoo2015@gmail.com>
2022-05-16 14:10:47 +0800

    useless files

--- modules/bot/events/client/ready.js
+++
@@ -1,8 +0,0 @@
-
-const { Client, Intents, Collection } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-module.exports = (Discord, client, message) =>{
-    console.log("event handler is up!");
-    client.user.setActivity("your Mom",{ type: 'WATCHING' });
-}
